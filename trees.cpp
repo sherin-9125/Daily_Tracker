a6163bde50ca2a662d7c52c029201870cd8985fd
Sherin Faurgana <sherinfaurgana09@gmail.com>
2026-07-19 10:37:57 +0530

    Binary tree traversals

--- trees.cpp
+++ trees.cpp
@@ -23,16 +23,45 @@ Node* buildtree(vector<int> preorder){
    return root;
 
 }
+// traversal-preorder (root,left,ryt)
+void preOrder( Node* root){
+  if(root==NULL){
 
+    return ;
+  }
+  cout<<root->data<<" ";
+  preOrder(root->left);
+  preOrder(root->right);
+
+}
+// traversal -inorder(left,root,right)
+void inorder( Node* root){
+  if(root==NULL){
+    return;
+  }
+  inorder(root->left);
+  cout<<root->data<<" ";
+  inorder(root->right);
+}
+// traversal -postorder(left,ryt,root) 
+void postorder( Node* root){
+  if(root==NULL){
+    return;
+  }
+  postorder(root->left);
+  postorder(root->right);
+  cout<<root->data<<" ";
+}
 
 
 int main(){
   vector<int> preorder={1,2,-1,-1,3,4,-1,-1,5,-1,-1};
    Node* root = buildtree(preorder);
-   cout<<root->data<<endl;
-   cout<<root->left->data<<endl;
-   cout<<root->right->data<<endl;
-
+  preOrder(root);
+  inorder(root);
+  postorder(root);
+    
+   
 
   return 0;
 }
