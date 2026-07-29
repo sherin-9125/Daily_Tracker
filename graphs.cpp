155771def63eedc22800de5b35fedd374fd0b37c
Sherin Faurgana <sherinfaurgana09@gmail.com>
2026-07-29 21:46:18 +0530

    bfs and dfs

--- graphs.cpp
+++ graphs.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<list>
+#include<queue>
 using namespace std;
 
 class Graph{
@@ -16,14 +17,46 @@ public:
   l[u].push_back(v);
   l[v].push_back(u);
  }
- void printList(){
-  for(int i=0;i<V;i++){
-    cout<<i << " : ";
-   for(int neigh :l[i]){
-    cout<<neigh<<" ";
-   }
-   cout<< endl;
+ void bfs(){
+  queue<int> Q;
+  vector<int> vis(V,false);
+
+  
+  Q.push(0);
+  vis[0]=true;
+  while(Q.size()>0){
+    int u=Q.front();
+    Q.pop();
+
+    cout<<u<<" ";
+    for(int v :l[u]){
+      if(!vis[v]){
+        vis[v]=true;
+        Q.push(v);
+
+      }
+    }
   }
+    cout<<endl;
+ }
+ void dfshelp(int u,vector<bool> vis){
+  cout<<u<<" ";
+  vis[u]=true;
+
+  for(int v:l[u]){
+    if(!vis[v]){
+      dfshelp(v,vis);
+    }
+  }
+
+ }
+ void dfs(){
+  int src=0;
+  vector <bool> vis(V,false);  // thid id for diconnected graph
+  for(int i=0;i<V;i++){        // 
+  dfshelp(i,vis);
+  }
+
  }
 };
 
@@ -31,10 +64,13 @@ int main(){
   Graph g(5);
   g.addEdge(0,1);
   g.addEdge(1,2);
-  g.addEdge(2,3);
+  g.addEdge(1,3);
   g.addEdge(2,4);
-  g.printList( );
-
+  cout<<"bfs:";
+  g.bfs();
+  cout<<"dfs:";
+  g.dfs();
+  
 
 
 }
